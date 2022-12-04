df682e586716488dc3a8a7f71a851056488d2828
Josh <j5d1c7@gmail.com>
2022-12-04 09:47:41 -0600

    day04

--- src/day04.zig
+++ src/day04.zig
@@ -9,8 +9,66 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day04.txt");
+// const data = @embedFile("data/day04_sample.txt");
 
-pub fn main() !void {}
+const Pair = struct {
+    low: usize,
+    high: usize,
+    len: usize,
+};
+
+fn parseGroup(pair: []const u8) !Pair {
+    var nums = tokenize(u8, pair, "-");
+    const low = try parseInt(usize, nums.next().?, 10);
+    const high = try parseInt(usize, nums.next().?, 10);
+    const len = high - low + 1;
+    return .{ .high = high, .low = low, .len = len };
+}
+
+pub fn main() !void {
+    var lines = tokenize(u8, data, "\n");
+
+    var sum1: usize = 0;
+    var sum2: usize = 0;
+
+    while (lines.next()) |line| {
+        var groups = tokenize(u8, line, ",");
+
+        const left = groups.next().?;
+        const right = groups.next().?;
+
+        var left_pair = try parseGroup(left);
+        var right_pair = try parseGroup(right);
+
+        { // complete overlap
+            var short_pair = &left_pair;
+            var long_pair = &right_pair;
+
+            if (long_pair.len < short_pair.len) {
+                std.mem.swap(Pair, short_pair, long_pair);
+            }
+
+            if (short_pair.low >= long_pair.low and short_pair.high <= long_pair.high) {
+                sum1 += 1;
+            }
+        }
+
+        { // partial overlap
+            var low_pair = &left_pair;
+            var high_pair = &right_pair;
+
+            if (low_pair.low > high_pair.low) {
+                std.mem.swap(Pair, low_pair, high_pair);
+            }
+
+            if (low_pair.low + low_pair.len > high_pair.low) {
+                sum2 += 1;
+            }
+        }
+    }
+
+    print("part1: {}, part2: {}\n", .{ sum1, sum2 });
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
