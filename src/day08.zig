a9241fd21ddb1f0ca66f7ff04f5e388977f2232e
Josh <j5d1c7@gmail.com>
2022-12-09 19:58:51 -0600

    day08 day09

--- src/day08.zig
+++ src/day08.zig
@@ -9,8 +9,176 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day08.txt");
+// const data = @embedFile("data/day08_sample.txt");
+const dataT = transpose(data);
 
-pub fn main() !void {}
+fn transpose(comptime in: []const u8) [in.len]u8 {
+    @setEvalBranchQuota(100000);
+    var result: [in.len]u8 = undefined;
+    const line_len = std.mem.sliceTo(in, '\n').len;
+
+    for (in) |c, i| {
+        if (c == '\n') {
+            result[i] = c;
+        } else {
+            const row = @divTrunc(i, line_len + 1);
+            const col = i % (line_len + 1);
+            result[col * (line_len + 1) + row] = c;
+        }
+    }
+
+    return result;
+}
+
+fn reverse(comptime in: []const u8) [in.len]u8 {
+    @setEvalBranchQuota(100000);
+    var result: [in.len]u8 = undefined;
+
+    var i: usize = 0;
+    while (i < in.len) : (i += 1) {
+        result[0] = in[in.len - 1 - i];
+    }
+
+    return result;
+}
+
+test "transpose" {
+    const in =
+        \\30373
+        \\25512
+        \\65332
+        \\33549
+        \\35390
+        \\
+    ;
+
+    const inT = transpose(in);
+    const a =
+        \\32633
+        \\05535
+        \\35353
+        \\71349
+        \\32290
+        \\
+    ;
+
+    try std.testing.expectEqualStrings(a, &inT);
+}
+
+fn getBitsetSize() usize {
+    @setEvalBranchQuota(100000);
+    const num_lines = std.mem.count(u8, data, "\n");
+    const line_len = std.mem.sliceTo(data, '\n').len;
+
+    return num_lines * line_len;
+}
+
+const Dir = enum {
+    forward,
+    backward,
+};
+
+const size = getBitsetSize();
+fn getVisibleSet(in: []const u8, dir: Dir, tpose: bool) std.StaticBitSet(size) {
+    var result = std.StaticBitSet(size).initEmpty();
+    var lines = std.mem.tokenize(u8, in, "\n");
+
+    var line_num: usize = 0;
+    while (lines.next()) |line| : (line_num += 1) {
+        var i: i32 = if (dir == .forward) 0 else @intCast(i32, line.len) - 1;
+        var inc: i32 = if (dir == .forward) 1 else -1;
+        var tallest: i32 = -1;
+
+        // print("{s} ->", .{line});
+        while (i >= 0 and i < line.len) : (i += inc) {
+            const ui = @intCast(usize, i);
+            const c = line[ui];
+            const height = parseInt(i32, &.{c}, 10) catch unreachable;
+            if (height > tallest) {
+                if (!tpose)
+                    result.set(line_num * line.len + ui)
+                else
+                    result.set(ui * line.len + line_num);
+
+                tallest = height;
+                // print("{c}, ", .{c});
+            }
+        }
+        // print("\n", .{});
+    }
+
+    return result;
+}
+
+fn score(in: []const u8, inT: []const u8, row: usize, col: usize) usize {
+    const len = std.mem.sliceTo(in, '\n').len;
+    const t = row * (len + 1) + col;
+    const tT = col * (len + 1) + row;
+
+    if (row == 0 or row == len - 1) return 0;
+    if (col == 0 or col == len - 1) return 0;
+
+    print("row {} {}\n", .{ row, col });
+
+    const east = std.mem.sliceTo(in[t..], '\n')[1..];
+    const west = in[(std.mem.lastIndexOf(u8, in[0..t], "\n") orelse std.math.maxInt(usize)) +% 1 .. t];
+
+    const south = std.mem.sliceTo(inT[tT..], '\n')[1..];
+    const north = inT[(std.mem.lastIndexOf(u8, inT[0..tT], "\n") orelse std.math.maxInt(usize)) +% 1 .. tT];
+
+    var scr: usize = 1;
+
+    const this_height = parseInt(usize, &.{in[t]}, 10) catch unreachable;
+
+    inline for (.{ east, west, south, north }) |line, i| {
+        print("line {s}\n", .{line});
+        var line_count: usize = 0;
+        var j: i32 = if (i % 2 == 0) 1 else -1;
+        var k: i32 = if (i % 2 == 0) 0 else @intCast(i32, line.len) - 1;
+
+        while (k >= 0 and k < line.len) : (k += j) {
+            const c = line[@intCast(usize, k)];
+            const height = parseInt(usize, &.{c}, 10) catch unreachable;
+            print("{} ", .{height});
+            line_count += 1;
+            if (height >= this_height) {
+                break;
+            }
+        }
+        print("\n", .{});
+
+        scr *= line_count;
+    }
+
+    return scr;
+}
+
+pub fn main() !void {
+    // bit is true if the tree can be seen
+    var looking_north = getVisibleSet(&dataT, .backward, true);
+    var looking_south = getVisibleSet(&dataT, .forward, true);
+    var looking_east = getVisibleSet(data, .forward, false);
+    var looking_west = getVisibleSet(data, .backward, false);
+
+    looking_east.setUnion(looking_west);
+    looking_east.setUnion(looking_north);
+    looking_east.setUnion(looking_south);
+
+    var prettiest: usize = 0;
+    const line_len = std.mem.sliceTo(data, '\n').len;
+
+    for (data) |c, i| {
+        if (c == '\n') continue;
+        const row = @divTrunc(i, line_len + 1);
+        const col = i % (line_len + 1);
+
+        const pretty = score(data, &dataT, row, col);
+        if (pretty > prettiest)
+            prettiest = pretty;
+    }
+
+    print("part1: {}, part2: {}\n", .{ looking_east.count(), prettiest });
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
--- src/day09.zig
+++ src/day09.zig
@@ -9,8 +9,130 @@ const util = @import("util.zig");
 const gpa = util.gpa;
 
 const data = @embedFile("data/day09.txt");
+// const data = @embedFile("data/day09_sample.txt");
 
-pub fn main() !void {}
+const Move = struct {
+    dir: u8,
+    repeat: usize,
+};
+
+fn getMoves() []Move {
+    var elems = std.mem.tokenize(u8, data, " \n");
+    var moves = ArrayList(Move).init(gpa);
+
+    while (elems.next()) |dir| {
+        const rep = parseInt(usize, elems.next().?, 10) catch unreachable;
+        moves.append(.{ .dir = dir[0], .repeat = rep }) catch unreachable;
+    }
+
+    return moves.toOwnedSlice() catch unreachable;
+}
+
+fn solveTail(head: Point, tail: Point) Point {
+    if (head.x == tail.x and head.y == tail.y) return tail;
+
+    const diffx = std.math.absInt(head.y - tail.y) catch unreachable;
+    const diffy = std.math.absInt(head.x - tail.x) catch unreachable;
+
+    if (diffx <= 1 and diffy <= 1) return tail;
+
+    var new = tail;
+
+    if (head.y == tail.y) {
+        new.x = head.x + std.math.sign(tail.x - head.x);
+        return new;
+    }
+
+    if (head.x == tail.x) {
+        new.y = head.y + std.math.sign(tail.y - head.y);
+        return new;
+    }
+
+    if (diffy == 1 and diffx == 0) {
+        new.y = head.y;
+        return new;
+    }
+
+    if (diffx == 1 and diffy == 0) {
+        new.x = head.x;
+        return new;
+    }
+
+    const movex = std.math.sign(head.x - tail.x);
+    const movey = std.math.sign(head.y - tail.y);
+
+    new.x += movex;
+    new.y += movey;
+
+    return new;
+}
+
+test "solve tail" {
+    const exe = std.testing.expectEqual;
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = 0 }, .{ .x = 0, .y = 0 }));
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 1, .y = 0 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 1, .y = 1 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = 1 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = -1, .y = 1 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = -1, .y = 0 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = -1, .y = -1 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = -1 }, .{ .x = 0, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 1, .y = -1 }, .{ .x = 0, .y = 0 }));
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 1, .y = 0 }, .{ .x = -1, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = -1, .y = 0 }, .{ .x = 1, .y = 0 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = 1 }, .{ .x = 0, .y = -1 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = -1 }, .{ .x = 0, .y = 1 }));
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 1, .y = 0 }, .{ .x = -1, .y = -1 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 1, .y = 0 }, .{ .x = -1, .y = 1 }));
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = 1 }, .{ .x = -1, .y = -1 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = 1 }, .{ .x = 1, .y = -1 }));
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = -1, .y = 0 }, .{ .x = 1, .y = -1 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = -1, .y = 0 }, .{ .x = 1, .y = 1 }));
+
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = -1 }, .{ .x = 1, .y = 1 }));
+    try exe(Point{ .x = 0, .y = 0 }, solveTail(.{ .x = 0, .y = -1 }, .{ .x = -1, .y = 1 }));
+}
+
+const Point = struct {
+    x: i32 = 0,
+    y: i32 = 0,
+};
+
+pub fn main() !void {
+    const moves = getMoves();
+
+    var knots = [_]Point{Point{}} ** 10;
+
+    var tailPositions = AutoHashMap(Point, void).init(gpa);
+
+    for (moves) |move| {
+        var i: usize = 0;
+        while (i < move.repeat) : (i += 1) {
+            switch (move.dir) {
+                'U' => knots[0].y -= 1,
+                'D' => knots[0].y += 1,
+                'R' => knots[0].x += 1,
+                'L' => knots[0].x -= 1,
+                else => unreachable,
+            }
+
+            for (knots) |*knot, j| {
+                if (j == 0) continue;
+                knot.* = solveTail(knots[j - 1], knot.*);
+                if (j == 9)
+                    tailPositions.put(knot.*, {}) catch unreachable;
+            }
+        }
+    }
+
+    print("part1: {}, part2: {}\n", .{ tailPositions.count(), 0 });
+}
 
 // Useful stdlib functions
 const tokenize = std.mem.tokenize;
